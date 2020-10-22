b3b748f9978ddf34fafbb5139f87ec99da33aa86
Diego Osvaldo <d.cruz@outlook.com>
2020-10-21 20:52:18 -0400

    Update all origins

--- civicos/settings.py
+++ civicos/settings.py
@@ -121,14 +121,15 @@ REST_FRAMEWORK = {
     )
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'https://localhost:4200',
-    'https://127.0.0.1:4200',
-    'http://localhost:4200',
-    'http://127.0.0.1:4200'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'https://localhost:4200',
+#     'https://127.0.0.1:4200',
+#     'http://localhost:4200',
+#     'http://127.0.0.1:4200'
+# ]
 # CORS_ORIGIN_WHITELIST = [
 #     'https://localhost:4200',
 #     'https://127.0.0.1:4200'
 # ]
+CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ORIGIN_ALLOW_ALL = True

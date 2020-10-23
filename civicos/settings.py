2dce1b0696da2900e3d00de2633389a547ed1274
Diego Osvaldo <d.cruz@outlook.com>
2020-10-22 23:09:47 -0400

    Update staticfiles

--- civicos/settings.py
+++ civicos/settings.py
@@ -101,7 +101,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles",
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 # STATICFILES_DIRS = (
